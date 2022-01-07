f8800e65dc562085268b5462c9bd0d0d5adc33d7
Jonas4420 <jonas.lejeune4420@gmail.com>
2022-01-07 18:05:46 +0900

    Add error messages

--- src/error.rs
+++ src/error.rs
@@ -15,8 +15,29 @@ pub enum GridError {
 
 impl fmt::Display for GridError {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        // TODO
-        write!(fmt, "")
+        write!(fmt, "error: ")?;
+
+        match self {
+            Self::AdjacentCells => {
+                write!(fmt, "3 consecutive cells have the same value")
+            }
+            Self::SameLanes => {
+                write!(fmt, "2 columns or 2 lines have the same value")
+            }
+            Self::EmptyGrid => {
+                write!(fmt, "parsed grid has no lines")
+            }
+            Self::LaneUnbalanced => {
+                write!(fmt, "a line has unbalanced number of 0s and 1s")
+            }
+            Self::InvalidChar(c) => {
+                write!(fmt, "unknown character '{}'", c)
+            }
+            Self::WidthMismatch(prev, curr) => {
+                write!(fmt, "a line has width {}, but previous ones are {}", curr, prev)
+            }
+            Self::IOError(err) => err.fmt(fmt),
+        }
     }
 }
 
