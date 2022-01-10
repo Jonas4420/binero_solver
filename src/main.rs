55b71c6456d8951b2e196f57cbbd6ca3a8b0a96c
Jonas4420 <jonas.lejeune4420@gmail.com>
2022-01-10 22:55:10 +0900

    Refresh main function

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,26 @@ mod error;
 mod grid;
 mod index;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO: refactor this code
+fn main() {
+    try_main().unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        std::process::exit(1);
+    });
+}
+
+fn try_main() -> Result<(), Box<dyn std::error::Error>> {
     let args = env::args().collect::<Vec<String>>();
-    let file = fs::File::open(&args[1])?;
+
+    if args.len() < 2 {
+        return Err(format!("usage: {} <FILE>", args[0]).into());
+    }
+
+    let file = fs::File::open(&args[1]).map_err(|err| format!("{}: {}", args[1], err))?;
     let reader = io::BufReader::new(file);
 
     let mut grid = grid::Grid::parse(reader.lines())?;
 
-    println!("Input:");
+    println!("Input grid:");
     println!("{}", grid);
 
     grid.solve()?;
